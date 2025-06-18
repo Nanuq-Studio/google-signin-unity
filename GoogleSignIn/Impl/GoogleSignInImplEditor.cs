57571579bebe90aa6a3262f447ba980d25eacbb5
Thara Mongkolwannalert <thara.nanuq@gmail.com>
2025-06-18 14:38:47 +0700

    - fix: escape uri string

--- GoogleSignIn/Impl/GoogleSignInImplEditor.cs
+++ GoogleSignIn/Impl/GoogleSignInImplEditor.cs
@@ -88,12 +88,12 @@ namespace Google.Impl
       var httpListener = BindLocalHostFirstAvailablePort();
       try
       {
-        var scopes = string.Join(" ", configuration.AdditionalScopes.Union(new string[] {
+        var scopes = Uri.EscapeDataString(string.Join(" ", configuration.AdditionalScopes.Union(new string[] {
           "openid",
           "email",
           "profile"
-        }));
-        var openURL = "https://accounts.google.com/o/oauth2/v2/auth?" + Uri.EscapeDataString($"scope={scopes}&response_type=code&redirect_uri={httpListener.Prefixes.FirstOrDefault()}&client_id={configuration.WebClientId}");
+        })));
+        var openURL = "https://accounts.google.com/o/oauth2/v2/auth?" + Uri.EscapeUriString($"scope={scopes}&response_type=code&redirect_uri={httpListener.Prefixes.FirstOrDefault()}&client_id={configuration.WebClientId}");
         Debug.Log(openURL);
         Application.OpenURL(openURL);
       }
